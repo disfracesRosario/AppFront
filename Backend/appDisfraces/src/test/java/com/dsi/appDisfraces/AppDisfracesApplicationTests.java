f57ccd344908f2bd1f03eb7380022dff3cc2ab19
agustin-kiryk <agustin.kiryk@gmail.com>
2023-03-04 16:01:49 -0300

    initial commit

---
+++ Backend/appDisfraces/src/test/java/com/dsi/appDisfraces/AppDisfracesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.appDisfraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDisfracesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
