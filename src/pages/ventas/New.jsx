e99e94c4b04e31782e8f5ae8e30de5ded8332e85
Agustin Kiryk <111461382+agustin-kiryk@users.noreply.github.com>
2023-06-11 16:35:35 -0300

    Of payment (#27)
    * editPDF
    
    * resolve image and pdf text
    
    * resolve rem
    
    * resolve rm
    
    * resolve amount modal

--- src/pages/ventas/New.jsx
+++ src/pages/ventas/New.jsx
@@ -18,6 +18,9 @@ import { DatePicker, Space } from 'antd';
 import { FormControlLabel, Checkbox } from '@material-ui/core';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
 import { saveAs } from 'file-saver';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+
+
 
 
 const Datatable = ({ singleId }) => {
@@ -41,8 +44,14 @@ const Datatable = ({ singleId }) => {
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [quantities, setQuantities] = useState({});
   const [selectedQuantities, setSelectedQuantities] = useState({});
+  const [open, setOpen] = useState(false);
+  const [data2, setData2] = useState(null);
+  const [clientName, setClientName] = useState('');
+  const [totalAmount, setTotalAmount] = useState(0);
+  
 
 
+  
   const handleTypeChange = (event) => {
     setType(event.target.value);
   };
@@ -50,7 +59,6 @@ const Datatable = ({ singleId }) => {
   const handleInvoiceChange = (event) => {
     setIsInvoiceChecked(event.target.checked);
   };
-
 
 
 
@@ -83,12 +91,19 @@ const Datatable = ({ singleId }) => {
   ];
 
 
-  const handleCostumeSelect = (productId, quantity) => {
+  const handleCostumeSelect = (productId, quantity, price, nameProduct) => {
     const newProduct = {
       productId: productId,
-      quantity: quantity
+      quantity: quantity,
+      price: price,
+      nameProduct: nameProduct,
     };
     setSelectedProducts([...selectedProducts, newProduct]);
+    setSelectedQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: quantity,
+    }));
+    
   };
   
 
@@ -99,6 +114,8 @@ const Datatable = ({ singleId }) => {
       setCheckIn(checkIn.filter((item) => item !== name));
     }
   };
+
+
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData();
@@ -128,27 +145,47 @@ const Datatable = ({ singleId }) => {
     const selectedRowsData = selectedRows.map((rowId) =>
       data.find((row) => row.id === rowId)
     );
-  
+
     const products = selectedRowsData.map((row) => ({
       productId: row.id,
       quantity: selectedQuantities[row.id] || 1,
+      price: row.price,
+      nameProduct: row.nameProduct,
+
+      
     }));
-  
+
     const requestData = {
       type,
       clientId,
       products: selectedProducts,
       checkIn: checkIn.join(','),
     };
-  
+
     const partialPaymentAmount = amount - partialPayment;
-  
+
+    setOpen(true);
+    setData2(requestData);
+    console.log(data2);
+    calculateTotalAmount();
+    const calculateTotalAmount = () => {
+      let total = 0;
+      selectedProducts.forEach((product) => {
+        total += product.price * product.quantity;
+      });
+      setTotalAmount(total);
+    };
+
+
+    
+
+
     axios
       .post('https://disfracesrosario.up.railway.app/transactions/newTransactionSale', requestData)
       .then(response => {
         const responseData = response.data;
         console.log(responseData);
-  
+
         const remitoData = {
           id: responseData.id,
           amount: responseData.amount,
@@ -158,66 +195,91 @@ const Datatable = ({ singleId }) => {
           checkIn: responseData.checkIn,
           clientName: responseData.clientName,
           clientLastName: responseData.clientLastName,
+          clientAdress: responseData.clientAdress,
+          clientDocument: responseData.clientDocument,
           transactionDetails: responseData.transactionDetails,
           billPayment: responseData.billPayment,
           statusPayment: responseData.statusPayment,
         };
+        
 
         const generateRemitoPDF = async (responseData) => {
           const pdfDoc = await PDFDocument.create();
-          const page = pdfDoc.addPage();
+          const page = pdfDoc.addPage([595.28, 841.89]);
           const { width, height } = page.getSize();
           const currentDate = new Date();
           const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
         
-          // Agregar contenido al PDF utilizando la biblioteca pdf-lib
-          page.drawText(`Fecha: ${formattedDate}`, { x: 50, y: height - 20 });
-          page.drawText(`ID: ${responseData.id}`, { x: 50, y: height - 50 });
-          page.drawText(`Monto: ${responseData.amount}`, { x: 50, y: height - 80 });
-          page.drawText(`Tipo: ${responseData.type}`, { x: 50, y: height - 110 });
-          page.drawText(`Nombre del cliente: ${responseData.clientName} ${responseData.clientLastName}`, { x: 50, y: height - 140 });
+          // Cargar la imagen de fondo
+          const imageUrl = 'https://res.cloudinary.com/dkzil7l5p/image/upload/v1686175894/plantillaRemito_edit_2_1_jgbxfz.png'; // URL de la imagen o ruta local
+          const imageBytes = await fetch(imageUrl).then((res) => res.arrayBuffer());
+          const backgroundImage = await pdfDoc.embedPng(imageBytes);
+          const fontSize = 9;
         
-          page.drawText("Detalles de la transacción:", { x: 50, y: height - 170 });
+          // Agregar contenido al PDF utilizando la biblioteca pdf-lib
+          page.drawImage(backgroundImage, { x: 0, y: 0, width: width, height: height, opacity: 1 });
+        
+          const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+          page.setFont(font);
+          page.setFontSize(fontSize);
+        
+          page.drawText(`Fecha: ${formattedDate}`, { x: 370, y: height - 195, fontSize });
+          page.drawText(`Remito N°\n   ${responseData.id}`, { x: 390, y: height - 140, fontSize });
+          page.drawText(`TOTAL : ${responseData.amount}`, { x: 365, y: height - 655, fontSize });
+          page.drawText(`Nombre del cliente: ${responseData.clientName} ${responseData.clientLastName}`, { x: 100, y: height - 227, fontSize });
+          page.drawText(`DNI: ${responseData.clientDocument}`, { x: 100, y: height - 241, fontSize });
+          page.drawText(`Direccion: ${responseData.clientAdress}`, { x: 100, y: height - 255, fontSize });
+          page.drawText(`Tipo: ${responseData.type}`, { x: 100, y: height - 269, fontSize });
+          page.drawText(`Telefono: ${responseData.type}`, { x: 100, y: height - 284, fontSize });
+          page.drawText(`identificafdor: ${responseData.clientId}`, { x: 100, y: height - 299, fontSize });
+          page.drawText(`Observaciones:  ${responseData.detail}`, { x: 100, y: height - 670, fontSize });
+          page.drawText("Detalles de la transacción:", { x: 10, y: height - 10, fontSize });
+
         
         
           responseData.transactionDetails.forEach((detail, index) => {
-            page.drawText(`Producto: ${detail.product}`, { x: 50, y: height - 200 });
-            page.drawText(`Cantidad: ${detail.quantity}`, { x: 50, y: height - 230 });
-            page.drawText(`Total Unitario: ${detail.totalUnitario}`, { x: 50, y: height - 260 });
-            page.drawText(`Total del Producto: ${detail.totalProduct}`, { x: 50, y: height - 290 });
+            page.drawText(`${detail.product}`, { x: 170, y: height - 360 - (20 * index), fontSize });
+            page.drawText(`${detail.quantity}`, { x: 320, y: height - 360 - (20 * index), fontSize });
+            page.drawText(`${detail.totalUnitario}`, { x: 365, y: height - 360 - (20 * index), fontSize });
+            page.drawText(`${detail.totalProduct}`, { x: 425, y: height - 360 - (20 * index), fontSize });
+            page.drawText(`${detail.productId}`, { x: 120, y: height - 360 - (20 * index), fontSize });
           });
         
           // Añadir más campos y detalles según sea necesario
-           
+        
           const pdfBytes = await pdfDoc.save();
         
           return pdfBytes;
         };
         
+        const generatePDFPreview = async (pdfBytes) => {
+          const pdfDataUri = 'data:application/pdf;base64,' + btoa(String.fromCharCode(...pdfBytes));
         
+          const viewer = document.getElementById('pdf-viewer');
+          viewer.src = pdfDataUri;
+        };
+        
+        // Generar el PDF del remito
+        generateRemitoPDF(responseData)
+          .then(pdfBytes => {
+            // Generar la vista previa del PDF
+            generatePDFPreview(pdfBytes);
+        
+            const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        
+            // Descargar el PDF en el navegador del usuario
+            //COMENTAR CUANDO NO SE QUIERE DESCARGAR PARA PROBAR
+           // saveAs(blob, 'remito.pdf');
+        
+            setCheckIn([]);
+          })
+          .catch(error => {
+            console.log('Error al generar el PDF:', error);
+          });
 
-
-           // Generar el PDF del remito
-           generateRemitoPDF(responseData)
-           .then(pdfBytes => {
-             const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-   
-             // Descargar el PDF en el navegador del usuario
-             saveAs(blob, 'remito.pdf');
-   
-             setCheckIn([]);
-             
-           })
-
-           
-
-      .catch(error => {
-        console.log('Error al generar el PDF:', error);
-      });
-  
         // Generar el remito utilizando remitoData
         console.log('Remito:', remitoData);
-  
+
         setCheckIn([]);
         alert("Venta realizada correctamente");
 
@@ -226,21 +288,6 @@ const Datatable = ({ singleId }) => {
         alert(error.response.data.details);
         setError(error.response.data.details);
       });
-  };
-  
-
-  const generateRemitoPDF = async (responseData) => {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage();
-    const { width, height } = page.getSize();
-  
-    // Agregar contenido al PDF utilizando la biblioteca pdf-lib
-    page.drawText(`ID: ${responseData.id}`, { x: 50, y: height - 50 });
-    // Añadir más campos y detalles según sea necesario
-  
-    const pdfBytes = await pdfDoc.save();
-  
-    return pdfBytes;
   };
 
 
@@ -260,6 +307,12 @@ const Datatable = ({ singleId }) => {
 
       // Mostrar las IDs que se van sumando en la consola
       console.log(`IDs seleccionadas: ${updatedIds.join(", ")}`);
+    } else{
+      // Actualiza las cantidades seleccionadas si la ID ya está seleccionada
+      setSelectedQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [id]: prevQuantities[id] + 1, // Incrementa la cantidad en 1
+      }));
     }
   };
 
@@ -273,6 +326,10 @@ const Datatable = ({ singleId }) => {
     setClientId(id);
   };
 
+  const handleNameChange = (name) => {
+    setClientName(name);
+  };
+
 
 
   return (
@@ -282,7 +339,7 @@ const Datatable = ({ singleId }) => {
         <a href="/">Volver a la pantalla</a>
       </button>
       <div className="info-cliente">
-        <BasicGrid onImageUrlChange={handleImageUrlChange} onIdChange={handleIdChange} />
+        <BasicGrid onImageUrlChange={handleImageUrlChange} onIdChange={handleIdChange} onNameChange={handleNameChange}  />
       </div>
       <div className="id">
       </div>
@@ -348,10 +405,41 @@ const Datatable = ({ singleId }) => {
       </div>
 
       <div className="final">
-        <Button onClick={handleAccept}>Aceptar</Button>
+        <Button onClick={() => setOpen(true)}>Aceptar</Button>
+        <Dialog open={open} onClose={() => setOpen(false)}>
+          <DialogTitle>Confirmación de la venta</DialogTitle>
+          <DialogContent>
+            {data2 && (
+              <div>
+                <h3>Verifique que los datos de la venta sean correctos:</h3>
+                <p>Monto: {data2.clientName}</p>
+                <p>Tipo: {data2.type}</p>
+                <p>Identificador de cliente: {data2.clientId}</p>
+                <p>Productos:</p>
+                <ul>
+                  {data2.products &&
+                    data2.products.map((product, index) => (
+                      <li key={index}>
+                         {selectedProducts.nameProduct} {product.price}, Cantidad: {product.quantity},  {totalAmount}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)} color="primary">
+              Cancelar
+            </Button>
+            <Button onClick={handleAccept} color="primary">
+              Aceptar
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
   );
-};
+}
+
 
 export default Datatable;
